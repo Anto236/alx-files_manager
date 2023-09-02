41a48c86931227fb773d75fcc74d1ec8b15335c1
Anto/236 <Antonymbugua1237@gmail.com>
2023-09-02 13:33:43 +0000

    task 1 correction

--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,14 @@
 import { env } from 'process';
 import { MongoClient, ObjectId } from 'mongodb';
 
+// eslint-disable-next-line import/prefer-default-export
 export class DBClient {
   constructor() {
-    const host = env.DB_HOST ? env.DB_HOST : '127.0.0.1';
-    const port = env.DB_PORT ? Number(env.DB_PORT) : 27017; // Ensure port is a number
+    const host = env.DB_PORT ? env.DB_PORT : '127.0.0.1';
+    const port = env.DB_HOST ? env.DB_HOST : 27017;
     const database = env.DB_DATABASE ? env.DB_DATABASE : 'files_manager';
-    this.myClient = new MongoClient(`mongodb://${host}:${port}`);
-  }
-
-  async connectToMongoDB() {
-    try {
-      await this.myClient.connect();
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-    }
+    this.myClient = MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.myClient.connect();
   }
 
   isAlive() {
@@ -23,24 +16,28 @@ export class DBClient {
   }
 
   async nbUsers() {
+    /* returns number of documents in the collection users */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.countDocuments();
   }
 
   async nbFiles() {
+    /* returns number of documents in the collection files */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('files');
     return myCollection.countDocuments();
   }
 
   async userExists(email) {
+    /* returns true if the user with the given email exists */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.findOne({ email });
   }
 
   async newUser(email, passwordHash) {
+    /* creates a new user with the given email and passwordHash */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.insertOne({ email, passwordHash });
@@ -50,6 +47,7 @@ export class DBClient {
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     if ('_id' in filters) {
+      // eslint-disable-next-line no-param-reassign
       filters._id = ObjectId(filters._id);
     }
     return myCollection.findOne(filters);
@@ -58,18 +56,12 @@ export class DBClient {
   async filterFiles(filters) {
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('files');
-    const idFilters = ['_id', 'userId', 'parentId'].filter(
-      (prop) => prop in filters && filters[prop] !== '0'
-    );
+    const idFilters = ['_id', 'userId', 'parentId'].filter((prop) => prop in filters && filters[prop] !== '0');
     idFilters.forEach((i) => {
+      // eslint-disable-next-line no-param-reassign
       filters[i] = ObjectId(filters[i]);
     });
     return myCollection.findOne(filters);
-  }
-
-  async close() {
-    await this.myClient.close();
-    console.log('MongoDB connection closed');
   }
 }
 
